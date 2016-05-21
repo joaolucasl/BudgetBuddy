92398d84012efd18e3cd7da7c3b6bfa3805927da
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2016-05-21 15:55:27 -0300

    Updated inthe build to add FXML files to the .jar

--- src/main/java/budget_buddy/App.java
+++ src/main/java/budget_buddy/App.java
@@ -1,7 +1,59 @@
-import org.javalite.activejdbc.Base;
+package budget_buddy;
 
-class App {
-    public static void main(String[] args){
-        System.out.println("Hello World!!");
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.*;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ * This is the main class for BudgetBuddy.
+ * It implements the JavaFX's abstract Application class,
+ * and is the basis for an MVC-like structure.
+ * At first it launches the LoginScreen, and the controllers
+ * define the subsequent steps.
+ *
+ * @author João Lucas Lucchetta
+ * @version 0.0.1
+ */
+public class App extends Application {
+    /**
+     * Launches the standalone JavaFX application
+     * starting from the scene defined in the {@link #start(Stage) start()} method.
+     *
+     * @param args The arguments passed to the application
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    /**
+     * The main entry point for our application.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * @param primaryStage the primary stage which will be shown once
+     *                     the application is run.
+     */
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    App.class.getResource("/budget_buddy/view/LoginScreen.fxml")
+            );
+            Pane root = (Pane) loader.load();
+
+            Scene loginScene = new Scene(root);
+
+            primaryStage.setScene(loginScene);
+            primaryStage.setTitle("Budget Buddy - Login");
+            primaryStage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
---
+++ src/main/java/budget_buddy/controller/LoginController.java
@@ -0,0 +1,7 @@
+package budget_buddy.controller;
+
+/**
+ * Created by hansolo on 21/05/16.
+ */
+public class LoginController {
+}
